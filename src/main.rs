522ff7d1c74864f18b61eb65785d5b2640919e4a
Eshe <ehinchliffe@proton.me>
2026-06-28 17:20:44 +0100

    Improved duration computation; now gives an error message when the given time comes before the current time; and it is more accurate (to the ms rather than to the s)

--- src/main.rs
+++ src/main.rs
@@ -32,13 +32,13 @@ enum Commands {
     }
 }
 
-fn wait_and_play(target_time: Instant) -> Result<(), Box<dyn std::error::Error>> {
+fn wait_and_play(time_to_wait: Duration) -> Result<(), Box<dyn std::error::Error>> {
     let mut stream_handle = rodio::DeviceSinkBuilder::open_default_sink()
         .expect("open default audio stream");
     stream_handle.log_on_drop(false);
     let player = rodio::Player::connect_new(&stream_handle.mixer());
     let source = Decoder::try_from(std::io::Cursor::new(&alarm))?;
-    sleep(target_time - Instant::now());
+    sleep(time_to_wait);
     player.append(source);
     player.sleep_until_end();
     Ok(())
@@ -46,19 +46,17 @@ fn wait_and_play(target_time: Instant) -> Result<(), Box<dyn std::error::Error>>
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let target_time: Instant;
+    let mut to_sleep_for: Duration = Duration::new(0, 0);
     match args.command {
         Commands::FromNow { to_wait: x } => {
-            let mut target = Instant::now();
             let mut time_to_wait = x.trim().split(':').rev().map(|x| x.parse::<u64>()).collect::<Vec<Result<u64, ParseIntError>>>().into_iter();
-            target += Duration::from_secs(time_to_wait.next().unwrap()?);
-            target += Duration::from_mins(time_to_wait.next().unwrap_or(Ok(0))?);
-            target += Duration::from_hours(time_to_wait.next().unwrap_or(Ok(0))?);
-            target_time = target;
+            to_sleep_for += Duration::from_secs(time_to_wait.next().unwrap()?);
+            to_sleep_for += Duration::from_mins(time_to_wait.next().unwrap_or(Ok(0))?);
+            to_sleep_for += Duration::from_hours(time_to_wait.next().unwrap_or(Ok(0))?);
         }
         Commands::AtTime { when: x } => {
-            let secs_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-            let time_to_start_of_day = secs_since_epoch % 86400;
+            let millis_since_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+            let time_to_start_of_day: u64 = (millis_since_epoch % 86400000).try_into().expect("The amount of milliseconds since the start of day exceeds u64::MAX");
             let hours_mins_secs = x.trim().split(':').collect::<Vec<&str>>();
             if hours_mins_secs.len() != 3 || hours_mins_secs.iter().any(|x| x.len() != 2) {
                 return Err("Must be in HH:MM:SS format".into());
@@ -66,17 +64,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let hour: u64 = hours_mins_secs[0].parse()?;
             let minute: u64 = hours_mins_secs[1].parse()?;
             let second: u64 = hours_mins_secs[2].parse()?;
-            let mut target = Instant::now();
-            target -= Duration::from_secs(time_to_start_of_day) + Duration::from_hours(1);
-            target += Duration::from_hours(hour) + Duration::from_mins(minute) + Duration::from_secs(second);
-            target_time = target;
+            to_sleep_for += Duration::from_hours(hour) + Duration::from_mins(minute) + Duration::from_secs(second);
+            if let Some(x) = to_sleep_for.checked_sub(Duration::from_millis(time_to_start_of_day) + Duration::from_hours(1)) {
+                to_sleep_for = x;
+            } else {
+                return Err("The given time should not be before the current time".into());
+            };
         }
     }
     if !args.no_daemon {
         match unsafe{fork()} {
             Ok(ForkResult::Parent{..}) => {},
             Ok(ForkResult::Child) => {
-                wait_and_play(target_time)?;
+                wait_and_play(to_sleep_for)?;
             },
             Err(y) => {
                 return Err(Box::new(y));
@@ -84,7 +84,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     else {
-        wait_and_play(target_time)?;
+        wait_and_play(to_sleep_for)?;
     }
     Ok(())
 }
